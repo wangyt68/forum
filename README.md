23b80b5bc21fd310ac5fdffa8287b1bf8b735b29
wangyt68 <476534288@qq.com>
2019-09-13 15:10:52 +0800

    add sustainable login

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# exercise
+#Demo
 
 ##Reference
 [Spring 文档](https://spring.io/guides)
+
 [github项目](https://github.com/wangyt68/forum)
+
 [GitHub Deploy keys](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+
 [GitHub Developer: OAuth](https://developer.github.com/apps/)
+
 [mybatis](http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
 
 ##Tool
+
+
+##script
+```sql
+create table USER
+(
+    ID           INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_9F2E7FD4_E121_4DE8_8F66_1D23B1C6E834) auto_increment,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+    constraint USER1_PK
+        primary key (ID)
+);
+```
--- src/main/java/com/wang/forum/controller/IndexController.java
+++ src/main/java/com/wang/forum/controller/IndexController.java
@@ -1,12 +1,37 @@
 package com.wang.forum.controller;
 
+import com.wang.forum.mapper.UserMapper;
+import com.wang.forum.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null)
+                {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
+//        @GetMapping("/")
+//            public String index() { return "index";}
+
