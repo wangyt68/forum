4cf0495a30d78ccedbb188ec28b56a77c4b7339d
wangyt68 <476534288@qq.com>
2019-09-18 14:49:45 +0800

    add the function of publishing the question

---
+++ src/main/java/com/wang/forum/mapper/QuestionMapper.java
@@ -0,0 +1,13 @@
+package com.wang.forum.mapper;
+
+import com.wang.forum.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag)" +
+            " values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
